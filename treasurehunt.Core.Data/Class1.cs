f2aa3c6b6a3acf05a382811776ac1781dd0c7b65
Fabio C <fabio.c.colaco@gmail.com>
2020-05-08 15:45:45 +0200

    Ajoutez des fichiers projet.

---
+++ treasurehunt.Core.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace treasurehunt.Core.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ treasurehunt.Web.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace treasurehunt.Web.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
