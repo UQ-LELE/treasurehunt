fa4f2fb3b99de18cf7699c8ac1a7002911dc4c03
Fabio C <fabio.c.colaco@gmail.com>
2020-05-13 18:52:08 +0200

    BackOffice => create Choice an Question controller + associated view create and edit + update Form for Create and Edit StoryEvent + update Action Create and Edit for StoryEvent controller

---
+++ treasurehunt.BackOffice.Web.UI/Controllers/ChoiceController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using treasurehunt.Core.Data.DataLayer;
+
+namespace treasurehunt.BackOffice.Web.UI.Controllers
+{
+    public class ChoiceController : Controller
+    {
+        #region Champs privés
+        private DalChoice _context = null;
+        #endregion
+
+        #region Constructors
+        public ChoiceController(DalChoice context)
+        {
+            this._context = context;
+        }
+        #endregion
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- treasurehunt.BackOffice.Web.UI/Controllers/StoryEventController.cs
+++ treasurehunt.BackOffice.Web.UI/Controllers/StoryEventController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using treasurehunt.Core.Data.DataLayer;
+using treasurehunt.Core.Data.Models.Quest;
 
 namespace treasurehunt.BackOffice.Web.UI.Controllers
 {
@@ -22,7 +23,53 @@ namespace treasurehunt.BackOffice.Web.UI.Controllers
 
         public IActionResult Index()
         {
+            List<StoryEvent> storyEvents = this._context.GetAllEvent();
+
+            return View(storyEvents);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatePost(StoryEvent eventToCreate)
+        {
+            IActionResult result = this.View(eventToCreate);
+
+            if (ModelState.IsValid)
+            {
+                this._context.AddEvent(eventToCreate);
+                result = RedirectToAction("Index");
+            }
+
+            return result;
+        }
+
+        public IActionResult Edit(int id)
+        {
+            StoryEvent eventToEdit = this._context.GetEventById(id);     
+
+            if(eventToEdit == null)
+            {
+                return NotFound();
+            }
+
+            return View(eventToEdit);
+        }
+
+        [HttpPost]
+        public IActionResult EditPost(StoryEvent eventToEdit)
+        {
+            IActionResult result = this.View(eventToEdit);
+
+            if (ModelState.IsValid)
+            {
+                this._context.EditEvent(eventToEdit);
+                result = RedirectToAction("Index");
+            }
+            return result;
         }
     }
 }
